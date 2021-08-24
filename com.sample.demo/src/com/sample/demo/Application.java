b2b9732cfb79191a8c313b514d5e6ff1bfa5d57d
Praveendass Selvaraj <Praveendass.Selvaraj@in.bosch.com>
2021-08-24 17:26:47 +0530

    Remove bin directory commit

---
+++ com.sample.demo/src/com/sample/demo/Application.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) Robert Bosch GmbH. All rights reserved.
+ */
+package com.sample.demo;
+
+
+/**
+ * @author SER4COB
+ *
+ */
+public class Application {
+
+  public static void main(String[] args) {
+    System.out.println("Hello to Github from praveen....");
+  }
+}
